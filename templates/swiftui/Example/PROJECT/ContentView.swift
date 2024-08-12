5f1417300a050984e9b5c7059dd9de9ef0e7ef51
bo.liu04 <bo.liu04@uim-solution.com>
2024-08-12 13:55:06 +0800

    Support SwiftUI

---
+++ templates/swiftui/Example/PROJECT/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PROJECT
+//
+//  Created by PROJECT_OWNER on TODAYS_YEAR/TODAYS_DATE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ templates/swiftui/Example/PROJECT/PROJECTApp.swift
@@ -0,0 +1,17 @@
+//
+//  PROJECTApp.swift
+//  PROJECT
+//
+//  Created by PROJECT_OWNER on TODAYS_YEAR/TODAYS_DATE.
+//
+
+import SwiftUI
+
+@main
+struct PROJECTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
